968f02cddba77261a4f682e63812c3ccdc257a26
Yaxyo <yaxyoraxmatullayev177@gmail.com>
2026-01-26 18:15:22 +0500

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -6,11 +6,11 @@ admin_router = Router()
 
 @admin_router.callback_query(F.data.in_(["accept_yes", "accpet_no"]))
 async def accept_ariza(callback: CallbackQuery):
-    if callback.data == "accept_no":
-        await callback.message.delete()
-        await callback.answer("Rad etildi", show_alert=True)
-    elif callback.data == "accept_yes":
-        new_text = callback.message.caption + "\n✅<i>Qabul qilingan</i>" 
-        await callback.message.edit_caption(
-            caption=new_text
-        )
+    new_text = callback.message.caption + "\n✅<i>Qabul qilingan</i>" 
+    await callback.message.edit_caption(caption=new_text)
+    
+
+@admin_router.callback_query(F.data == "accpet_no")
+async def accept_ariza(callback: CallbackQuery):
+    await callback.message.delete()
+    await callback.answer("Rad etildi", show_alert=True)
